31dd1c74e8ea6b1e79693b17d5097d36f7e7f7ff
unknown <151481930+Sebas-lop@users.noreply.github.com>
2024-04-04 09:37:10 -0500

    actualizacion de mes constructor

--- semana-4/Date/Date.h
+++ semana-4/Date/Date.h
@@ -25,13 +25,13 @@ public:
             else if(m==2 && d>=1 && d<=28){
                 day=d;
             }
-            else if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
+            else if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12){
                 if(d>=1 && d<=31){
                 day=d;
 
                 }
             }
-            else if(month==4 || month==6 || month==8 || month==9 || month==11){
+            else if(m==4 || m==6 || m==8 || m==9 || m==11){
                 if(d>=1 && d<=30){
                 day=d;
 
